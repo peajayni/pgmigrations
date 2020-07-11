322547d1153a8685c7ab2d8c3791048a06061094
peajayni <peajayni>
2020-07-11 16:00:58 +0100

    Add fallback version

--- ci/set_version_from_git_tag.sh
+++ ci/set_version_from_git_tag.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eufx
 
-version=$(git tag | tail -1)
+package="pgmigrations"
+version_file="${package}/version.py"
 
-echo "__version__ = \"${version}\"" > pgmigrations/version.py
+fallback_version="0.0.0"
+git_version=$(git tag | tail -1)
+version=${git_version:=${fallback_version}}
+
+
+echo "__version__ = \"${version}\"" > ${version_file}
